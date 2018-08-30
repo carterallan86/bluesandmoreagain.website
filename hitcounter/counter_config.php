4a618b3440b50de2ef43c735f509d03a66cc440c
carterallan86 <carterallan86@gmail.com>
2018-08-30 10:48:39 +0100

    test

--- hitcounter/counter_config.php
+++ hitcounter/counter_config.php
@@ -13,6 +13,6 @@ $dbtableinfo= 'info';
 $maxrows = 50; // Restrics how many entry´s are allowed in $dbtableinfo. if more then $maxrows , new entry´s will replace the oldest to keep your database small. 
 // Run the actual connection here  
 $link = mysqli_connect($db_host, $db_username, $db_pass, $db_name);
-
+test
 
 ?>
